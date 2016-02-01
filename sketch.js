dfcdcb744f5de561b2137f31f7f23e807f126a89
NinjAdina <AdinaShuttari@Adinas-MacBook-Pro.local>
2016-01-31 22:09:49 -0500

    House = Object
    Moved House from under to above setup() in order to make it its own
    object. But structure is still under house as rect and roof as triangle.

--- sketch.js
+++ sketch.js
@@ -4,36 +4,18 @@ var house = {
     width: 200, // was houseWidth
     height: 200, // was houseHeight
     roofHeight: 100,
-    draw: function() {
-        this.drawStructure();
-        this.drawRoof();
-    },
-    drawStructure: function() {
+    draw: function() { // this is nearly identical to drawHouse(); what's different?
         rect(this.x, this.y, this.width, this.height); // Draw a structure
-    },
-    drawRoof: function() {
-      var leftEaves = {
-      x: this.x - 10,
-      y: this.y
-    };
-    var roofPoint = {
-      x: this.width / 2 + this.x,
-      y: this.y - this.roofHeight
-    };
-    var rightEaves = {
-      x: this.x + this.width + 10,
-      y: this.y
-    };
-    triangle(leftEaves.x, leftEaves.y, roofPoint.x, roofPoint.y, rightEaves.x, rightEaves.y);
-  }
+        triangle(this.x - 10, this.y, this.width / 2 + this.x, this.y - this.roofHeight, this.x + this.width + 10, this.y);
+    }
 };
 
 var setup = function setup() {
   createCanvas(800, 800); // We still need a canvas
 
-  house.draw(); // first house.
+  house.draw();
   house.x = house.x + 250;
-  house.draw(); // second house.
+  house.draw();
 }
 
 var draw = function () {
